a11909593c1c6450502b4a4d482b7000ef9a6571
Lukács Szabolcs <szabolcs.lukacs@webapix.hu>
2021-11-30 18:10:08 +0200

    Frontend with fakedata

--- lib/tournament_details_screen.dart
+++ lib/tournament_details_screen.dart
@@ -3,6 +3,26 @@ import 'package:flutter/material.dart';
 import './beerpontext.dart';
 import './buttons.dart';
 
+class Team extends StatelessWidget{
+  const Team({Key? key, required this.team_name}) : super(key: key);
+  final String team_name;
+  //print(team_name);
+  @override
+  Widget build(BuildContext context)
+  {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center, //Center Row contents horizontally,
+      crossAxisAlignment: CrossAxisAlignment.center, //Center Row contents vertically,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(25.0),
+              child: Text(team_name),
+        ),
+      ],
+    );
+  }
+}
+
 
 class TournamentScreenDetails extends StatelessWidget {
   const TournamentScreenDetails({Key? key, required this.tournament}) : super(key: key);
@@ -15,14 +35,52 @@ class TournamentScreenDetails extends StatelessWidget {
       appBar: AppBar(
         title: Text(tournament.name),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text(tournament.name),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center, //Center Row contents horizontally,
+                crossAxisAlignment: CrossAxisAlignment.center, //Center Row contents vertically,
+              children: const [
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Team(team_name: 'Sorkiralyok'),
+                Team(team_name: 'Sorkiralyok'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Team(team_name: 'Gyoztes'),
+              ],
+            ),
           ],
         ),
-      ),
+
     );
   }
 }
