c7bcd93f4d71cb9ddafeb9b07200a7f1f073203b
Lukács Szabolcs <szabolcs.lukacs@webapix.hu>
2021-12-10 23:12:48 +0200

    Create example database

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,43 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:bptracker_sqlite/page/notes_page.dart';
 import 'package:flutter/material.dart';
 import './beerpontext.dart';
 import './loginscreen.dart';
 import './buttons.dart';
+import 'package:flutter/services.dart';
+import 'package:bptracker_sqlite/page/notes_page.dart';
 
 
 void main() async
 {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  static final String title = 'Notes SQLite';
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    themeMode: ThemeMode.dark,
+    theme: ThemeData(
+      primaryColor: Colors.black,
+      scaffoldBackgroundColor: Colors.blueGrey.shade900,
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      home: LoginScreen(),
-    );
-  }
+    ),
+    home: NotesPage(),
+  );
 }
+
